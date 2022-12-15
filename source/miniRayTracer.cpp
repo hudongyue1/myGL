ff6b84e0e2a1e1c3a3716880478b78e28b262cf0
hudongyue1 <2535593261@qq.com>
2022-12-15 15:30:45 +0800

    add support for render object Triangle

--- source/miniRayTracer.cpp
+++ source/miniRayTracer.cpp
@@ -21,6 +21,8 @@
 // get the max float as infinity
 const float kInfinity = std::numeric_limits<float>::max();
 
+constexpr float kEpsilon = 1e-8;
+
 // get random
 std::random_device rd;
 std::mt19937 gen(rd());
@@ -70,6 +72,9 @@ bool solveQuadratic(const float &a, const float &b, const float &c, float &x0, f
 
 }
 
+/**
+ *  support classes : Sphere, Triangle, Plane, Disk, Box
+ */
 class Sphere : public Object {
 private:
     float radius, radius2;
@@ -110,6 +115,95 @@ public:
     }
 };
 
+class Triangle : public Object {
+private:
+    Vec3f vertex0, vertex1, vertex2;
+    Vec3f normal;
+public:
+    Triangle(const Vec3f &v0, const Vec3f &v1, const Vec3f &v2) : vertex0(v0), vertex1(v1), vertex2(v2) {
+        Vec3f v0v1 = vertex1 - vertex0;
+        Vec3f v0v2 = vertex2 - vertex0;
+        normal = v0v1.crossProduct(v0v2);
+        normal.normalize();
+    }
+
+    bool intersect(const Vec3f &orig, const Vec3f &dir, float &t) const {
+        // geometry solution
+        Vec3f v0v1 = vertex1 - vertex0;
+        Vec3f v0v2 = vertex2 - vertex0;
+
+        // exclude parallel
+        float normalDotRayDirection = normal.dotProduct(dir);
+        if(abs(normalDotRayDirection) < kEpsilon)
+            return false;
+
+        // compute t for intersection
+        float D = -(normal.dotProduct(vertex0));
+        t = -(normal.dotProduct(orig) + D) / normalDotRayDirection;
+
+        // exclue t < 0, intersection behind the origin of the ray
+        if(t < 0) return false;
+
+        // get the hit point
+        Vec3f Phit = orig + t*dir;
+
+        Vec3f v0P = Phit - vertex0;
+        Vec3f v1P = Phit - vertex1;
+        Vec3f v2P = Phit - vertex2;
+
+        // judge inside or not
+        Vec3f J;
+        J = v0v1.crossProduct(v0P);
+        if(normal.dotProduct(J) < 0) return false;
+
+        Vec3f v1v2 = vertex2 - vertex1;
+        J = v1v2.crossProduct(v1P);
+        if(normal.dotProduct(J) < 0) return false;
+
+        Vec3f v2v0 = vertex0 - vertex2;
+        J = (-v0v2).crossProduct(v2P);
+        if(normal.dotProduct(J) < 0) return false;
+        // Step 2: inside-outside test
+//        Vec3f C;  //vector perpendicular to triangle's plane
+//
+//        // edge 0
+//        Vec3f edge0 = vertex1 - vertex0;
+//        Vec3f vp0 = Phit - vertex0;
+//        C = edge0.crossProduct(vp0);
+//        if (normal.dotProduct(C) < 0) return false;  //P is on the right side
+//
+//        // edge 1
+//        Vec3f edge1 = vertex2 - vertex1;
+//        Vec3f vp1 = Phit - vertex1;
+//        C = edge1.crossProduct(vp1);
+//        if (normal.dotProduct(C) < 0)  return false;  //P is on the right side
+//
+//        // edge 2
+//        Vec3f edge2 = vertex0 - vertex2;
+//        Vec3f vp2 = Phit - vertex2;
+//        C = edge2.crossProduct(vp2);
+//        if (normal.dotProduct(C) < 0) return false;  //P is on the right side;
+
+        return true;
+    }
+
+    void getSurfaceData(const Vec3f &Phit, Vec3f &Nhit, Vec2f &tex) const {
+        Nhit = normal;
+        tex.x = (1 + atan2(Nhit.z, Nhit.x) / M_PI) * 0.5;
+        tex.y = acosf(Nhit.y) / M_PI;
+    }
+};
+
+
+/**
+ * Test each object for intersection
+ * @param orig
+ * @param dir
+ * @param objects
+ * @param tNear
+ * @param hitObject
+ * @return
+ */
 bool trace(const Vec3f &orig, const Vec3f &dir, const std::vector<std::unique_ptr<Object>> &objects, float &tNear, const Object *&hitObject) {
     tNear = kInfinity;
     std::vector<std::unique_ptr<Object>>::const_iterator iter = objects.begin();
@@ -124,6 +218,13 @@ bool trace(const Vec3f &orig, const Vec3f &dir, const std::vector<std::unique_pt
     return (hitObject != nullptr);
 }
 
+/**
+ * Cast a ray with specific origin and direction, then compute the color in hit point.
+ * @param orig
+ * @param dir
+ * @param objects
+ * @return
+ */
 Vec3f castRay(const Vec3f &orig, const Vec3f &dir, const std::vector<std::unique_ptr<Object>> &objects) {
     Vec3f hitColor = 0;
     const Object *hitObject = nullptr;
@@ -142,6 +243,11 @@ Vec3f castRay(const Vec3f &orig, const Vec3f &dir, const std::vector<std::unique
     return hitColor;
 }
 
+/**
+ * Render the whole scene with options and objects info
+ * @param options
+ * @param objects
+ */
 void render(const Options &options, const std::vector<std::unique_ptr<Object>> &objects) {
     Vec3f *framebuffer = new Vec3f[options.width * options.height];
     Vec3f *pix = framebuffer;
@@ -164,7 +270,7 @@ void render(const Options &options, const std::vector<std::unique_ptr<Object>> &
         }
     }
 
-    std::ofstream ofs("../out/out.ppm", std::ios::out | std::ios::binary);
+    std::ofstream ofs("./out/out.ppm", std::ios::out | std::ios::binary);
     ofs << "P6\n" << options.width << " " << options.height << "\n225\n";
     for(uint32_t i=0; i<options.height * options.width; ++i) {
         char r = (char)(255*clamp(0, 1, framebuffer[i].x));
@@ -181,13 +287,17 @@ void render(const Options &options, const std::vector<std::unique_ptr<Object>> &
 int main(int argc, char **argv) {
     std::vector<std::unique_ptr<Object>> objects;
 
-    uint32_t numSpheres = 32;
+    uint32_t numSpheres = 3;
     gen.seed(0);
-    for (uint32_t i = 0; i < numSpheres; ++i) {
-        Vec3f randPos((0.5 - dis(gen)) * 10, (0.5 - dis(gen)) * 10, (0.5 + dis(gen) * 10));
-        float randRadius = (0.5 + dis(gen) * 0.5);
-        objects.push_back(std::unique_ptr<Object>(new Sphere(randPos, randRadius)));
-    }
+//    for (uint32_t i = 0; i < numSpheres; ++i) {
+//        Vec3f randPos((0.5 - dis(gen)) * 10, (0.5 - dis(gen)) * 10, (0.5 + dis(gen) * 10));
+//        float randRadius = (0.5 + dis(gen) * 0.5);
+//        objects.push_back(std::unique_ptr<Object>(new Sphere(randPos, randRadius)));
+//    }
+    Vec3f v0((0.5 - dis(gen)) * 10, (0.5 - dis(gen)) * 10, (0.5 + dis(gen) * 10));
+    Vec3f v1(0.5 * 10, (0.5 - dis(gen)) * 10, (0.5 + dis(gen) * 10));
+    Vec3f v2((0.5 - dis(gen)) * 10, 0.5 * 10, (0.5 + dis(gen) * 10));
+    objects.push_back(std::unique_ptr<Object>(new Triangle(v0, v1, v2)));
 
     // setting up options
     Options options;
